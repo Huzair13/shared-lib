09753707ee22d8bcaf78b6fbd80121c7a03c05d9
Huzair13 <ahamedhuzair13@gmail.com>
2024-12-13 18:06:59 +0530

    first commit

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,21 @@
+def login() {
+    withCredentials([usernamePassword(credentialsId: '80bc73ba-89dc-4de0-8cde-ddcf63e85680', usernameVariable: 'username', passwordVariable: 'password')]) {
+        sh """
+            docker login --username="${username}" --password="${password}"
+        """
+    }
+}
+
+def build(String tag,String file_name) {
+    def scriptcontents = libraryResource "Dockerfile"
+    writeFile file:"Dockerfile", text: scriptcontents
+    sh """
+        docker build --build-arg file_name="${file_name}" -t "${tag}"  .
+    """
+}
+
+def push(String tag) {
+    sh """
+        docker push "${tag}"
+    """
+}
